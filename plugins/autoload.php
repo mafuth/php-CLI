6f8008261b3966613904bb05adc72efb32e1b95b
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2021-12-12 17:54:34 +0500

    Update autoload.php

--- plugins/autoload.php
+++ plugins/autoload.php
@@ -1,12 +1,2 @@
 <?php
-$PLUGINS = scandir(__DIR__);
-$i=2;
-while($PLUGINS[$i] != ""){
-    if($PLUGINS[$i] != "autoload.php"){
-        $PLUGIN_DATA = json_decode(file_get_contents($PLUGINS[$i].'/infomation.json'),true)[0];
-        if(str_ireplace(' ','-',$PLUGIN_DATA[0]['name']) == $COMAND[2]){
-            include($PLUGINS[$i].'/'.$PLUGIN_DATA[0]['loader']);
-        }
-        $i++;
-    }
-}
+//this file is depriciated
