34734260418f832943fdfbb60b809ebd35f33276
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2022-08-25 10:22:49 +0500

    Update device.class.php

--- classes/device.class.php
+++ classes/device.class.php
@@ -149,5 +149,12 @@ class device{
 			return 'Computer';
 		}   
 	}
+	public function isBot(){
+		if (!preg_match('/bot|crawl|curl|dataprovider|search|get|spider|find|java|majesticsEO|google|yahoo|teoma|contaxe|yandex|libwww-perl|facebookexternalhit/i', $_SERVER['HTTP_USER_AGENT'])) {
+			return false;
+		}else{
+			return true;
+		}
+	}
 
 }
