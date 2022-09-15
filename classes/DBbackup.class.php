f2b23ccb05e3d9617af035020d73a524e1e3635e
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2022-09-15 10:13:35 +0500

    Update DBbackup.class.php

--- classes/DBbackup.class.php
+++ classes/DBbackup.class.php
@@ -10,6 +10,47 @@ class DBbackup {
             die("Error ".$e->getMessage());
         }
     }
+
+    public function saveLayout($tables = '*'){
+        $output = "-- database backup - ".date('Y-m-d H:i:s').PHP_EOL;
+        $output .= "SET NAMES utf8;".PHP_EOL;
+        $output .= "SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';".PHP_EOL;
+        $output .= "SET foreign_key_checks = 0;".PHP_EOL;
+        $output .= "SET AUTOCOMMIT = 0;".PHP_EOL;
+        $output .= "START TRANSACTION;".PHP_EOL;
+        //get all table names
+        if($tables == '*') {
+            $tables = [];
+            $query = $this->dbInstance->prepare('SHOW TABLES');
+            $query->execute();
+            while($row = $query->fetch(PDO::FETCH_NUM)) {
+                $tables[] = $row[0];
+            }
+            $query->closeCursor();
+        }
+        else {
+            $tables = is_array($tables) ? $tables : explode(',',$tables);
+        }
+
+        foreach($tables as $table) {
+            $query = $this->dbInstance->prepare("SELECT * FROM `$table`");
+            $query->execute();
+            $output .= "DROP TABLE IF EXISTS `$table`;".PHP_EOL;
+
+            $query2 = $this->dbInstance->prepare("SHOW CREATE TABLE `$table`");
+            $query2->execute();
+            $row2 = $query2->fetch(PDO::FETCH_NUM);
+            $query2->closeCursor();
+            $output .= PHP_EOL.$row2[1].";".PHP_EOL;
+        }
+        $output .= PHP_EOL.PHP_EOL;
+
+        $output .= "COMMIT;";
+        //save filename
+
+        $filename = 'storage/db_layout.sql';
+        $this->writeUTF8filename($filename,$output);
+    }
    
     public function backup($tables = '*'){
         $output = "-- database backup - ".date('Y-m-d H:i:s').PHP_EOL;
@@ -64,6 +105,7 @@ class DBbackup {
 
         $filename = 'storage/db_backup.sql';
         $this->writeUTF8filename($filename,$output);
+        $this->saveLayout();
     }
    
    
