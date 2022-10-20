3479b34da4e2f89bbe2badd1ec93a93461227b1c
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2022-10-20 11:57:53 +0500

    Update main.php

--- main.php
+++ main.php
@@ -14,14 +14,16 @@ if(file_exists(__DIR__.'/config.ini')){
 }else{
     error_reporting(0);
 }
+
+//classes loader
+include (__DIR__.'/includes/autoload.inc.php');
+
 if(file_exists(__DIR__.'/vendor/autoload.php')){
     include(__DIR__.'/vendor/autoload.php');
     if($config['mailServer'] !=""){
         $mailer =  new mailer($config['mailServer'], $config['mailUsername'], $config['mailPassword'], $config['mailport']);
     }
 }
-//classes loader
-include (__DIR__.'/includes/autoload.inc.php');
 
 //security funtions
 $SECURITY = new security ($config['KEY_ONE'],$config['KEY_TWO']);
