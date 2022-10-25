6ce8adcd302a56f62c898557925028d541029b99
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2022-10-25 21:29:02 +0500

    Update compiler.php

--- app/compiler.php
+++ app/compiler.php
@@ -8,37 +8,30 @@ class compiler{
         return gzuncompress($data);
     }
 
-    public function minifyOutput($input){
-        
-        $search = array(
-            '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
-            '/[^\S ]+\</s',     // strip whitespaces before tags, except space
-            '/(\s)+/s',         // shorten multiple whitespace sequences
-            '/<!--(.|\s)*?-->/' // Remove HTML comments
-        );
-        
-        $replace = array(
-            '>',
-            '<',
-            '\\1',
-            ''
-        );
-        $buffer = $input;
+    public function output($input,$min){
         if($min == true){
-            $buffer = preg_replace($search, $replace, $buffer);
-            $buffer = str_replace(array("\r\n", "\r", "\n"), "", $buffer);
+            $search = array(
+                '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+                '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+                '/(\s)+/s',         // shorten multiple whitespace sequences
+                '/<!--(.|\s)*?-->/' // Remove HTML comments
+            );
+            
+            $replace = array(
+                '>',
+                '<',
+                '\\1',
+                ''
+            );
+            $buffer = $input;
+            if($min == true){
+                $buffer = preg_replace($search, $replace, $buffer);
+                $buffer = str_replace(array("\r\n", "\r", "\n"), "", $buffer);
+            }
+            $buffer = str_replace('<form', '<form enctype="multipart/form-data"', $buffer);
+            $buffer = str_replace('</form>', '<input type="hidden" name="csrf" value="'.$_SESSION['token'].'"></form>', $buffer);
+            $buffer = str_replace('<img', '<img loading="lazy"', $buffer);
         }
-        $buffer = str_replace('<form', '<form enctype="multipart/form-data"', $buffer);
-        $buffer = str_replace('</form>', '<input type="hidden" name="csrf" value="'.$_SESSION['token'].'"></form>', $buffer);
-        $buffer = str_replace('<img', '<img loading="lazy"', $buffer);
-        
-        return $buffer;
-    }
-
-    public function output($data,$min){
-        if($min == true){
-            $data = $this->minifyOutput($data);
-        }
-        echo $data;
+        echo '<!doctype html><html lang="en">'.$buffer.'</html>';
     }
 }
