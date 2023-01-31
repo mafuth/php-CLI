3524ed6d477c102d50a3b00a9218d504c61c6bd5
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2023-01-31 19:58:04 +0500

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -23,4 +23,28 @@ class routes {
         }
 
     }
+
+    public function redirect($url,$type,$message){
+        if(!empty($type)){
+            $_SESSION[$type] = $message;
+        }
+        header('Location: '.$url);
+    }
+
+    public function issetMessage($type){
+        $message = false;
+        if(!empty($_SESSION[$type])){
+            $message = true;
+        }
+        return $message;
+    }
+
+    public function getMessage($type){
+        $message = "";
+        if(!empty($_SESSION[$type])){
+            $message = $_SESSION[$type];
+            $_SESSION[$type] = "";
+        }
+        return $message;
+    }
 }
